c486ff320994da0184c57228912dc7d22ef2a4ef
IndominousRex <aritrasaha33@gmail.com>
2025-11-22 18:15:47 +0100

    Typo fixed

--- code/scripts/run_npe_gpu.sh
+++ code/scripts/run_npe_gpu.sh
@@ -38,7 +38,7 @@ module load Miniforge3
 conda activate /software/NHKB22930/nhkbarit/conda_envs/npe
 
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-export JAX_PLATFORM_NAME=gpu
+export JAX_PLATFORM_NAME=cpu
 
 # -------------------
 # RUN THE EXPERIMENT
