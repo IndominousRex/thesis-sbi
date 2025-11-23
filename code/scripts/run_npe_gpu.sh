315e44d57c7d199b7b96e90779b0b360568feb34
IndominousRex <aritrasaha33@gmail.com>
2025-11-23 11:47:07 +0100

    Changed script config

--- code/scripts/run_npe_gpu.sh
+++ code/scripts/run_npe_gpu.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:a100m40:4
+#SBATCH --mem-per-cpu=8G
 #SBATCH --time=04:00:00
 #SBATCH --output=npe_gpu_%A_%a.out
 #SBATCH --error=npe_gpu_%A_%a.err
