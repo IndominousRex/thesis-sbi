ad8e353a4a5d7b6a7fccf57198a5751560d22b47
IndominousRex <aritrasaha33@gmail.com>
2025-12-14 03:57:09 +0100

    Modified code for transformer cpu test run

--- code/scripts/run_npe_cpu_transformer.sh
+++ code/scripts/run_npe_cpu_transformer.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=512G
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --time=12:00:00
 #SBATCH --output=npe_cpu_%j.out
 #SBATCH --error=npe_cpu_%j.err
